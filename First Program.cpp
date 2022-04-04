c4199ffbcab28ec852d382f00fb0bfe66dd256c8
Jaime Ramirez <74738042+ZIM-Hub@users.noreply.github.com>
2022-04-03 21:35:56 -0600

    Create First Program.cpp

---
+++ First Program.cpp
@@ -0,0 +1,16 @@
+/*This was my first program i created and was my first assignment
+
+--Creat a program that outputs "Hello world!
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
